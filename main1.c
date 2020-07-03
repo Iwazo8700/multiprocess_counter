f28dea2733409e75c6aaef0a0520a9e4fff66b15
Enzo_Iwata <enzo.iwata8700@gmail.com>
2020-07-03 17:39:03 -0300

    programa 10/10 e comentado

--- main1.c
+++ main1.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
+int primo(int n){
+    //printf("entrei no primo %d\n", n);    
+    if(n < 2) return 0;
+    for(int i=2; i<n; i++){
+        if(n % i == 0) return 0;
+        }
+    //printf("1\n");
+    return 1;
+    }
+
+
 int main(){
-    /*
-    int num;
     char c;
-
-    while(scanf("%d%c", &num, &c)){
-      
-        printf("%d\n", num);
-        if(c == '\n') break;
-    }*/
-    printf("%d\n", 5%1);
-
+    int i;
+    int tam = 0;
+    while(scanf("%d%c", &i, &c)){
+        tam += primo(i);
+        if(c=='\n') break;
+    }
+    printf("%d\n", tam);
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -12,32 +12,42 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <signal.h>
+
 #define NUM_PROC 4
 
 
 
 
 int primo(int n){
-    //printf("entrei no primo %d\n", n);    
+    /*funcao que verifica se um numero eh primo ou nao
+      funcao nao esta otimizada, portanto ele faz o calculo
+      verifica a divisao inteira de todos os numeros inteiros
+       menores que n e maiores que 0.*/
+
     if(n < 2) return 0;
     for(int i=2; i<n; i++){
         if(n % i == 0) return 0;
         }
-    //printf("1\n");
+    
     return 1;
     }
 
-void sigquit(){
-        //printf("sigquit\n");
-        exit(0);
-    }
-int all_proc(int *proc){
-    
-        for(int i=0; i<NUM_PROC; i++)
-            if(!proc[i]) return 0;
-        return 1;
-    }
 
+void sigquit(){
+    /*funcao que recebe um sinal e encerra o processo*/
+    exit(0);
+}
+
+
+int all_proc(int *proc){
+    /*funcao que verifica se todos os valores dentro de um vetor
+      sao verdadeiros. No caso desse codigo sele serve para verificar
+      se ja iniciei a quantidade de processos desejada*/
+
+    for(int i=0; i<NUM_PROC; i++)
+        if(!proc[i]) return 0;
+    return 1;
+}
 
 int main() {
     pid_t filho[NUM_PROC];
@@ -45,6 +55,7 @@ int main() {
     char c;
     int protection = PROT_READ | PROT_WRITE;
     int visibility = MAP_SHARED | MAP_ANON;
+    /*variaveis mmap para q todos os processos tenham acesso*/
     int *count, *numero, *init, *processo;
     count = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
     numero = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
@@ -52,51 +63,61 @@ int main() {
     init = (int*) mmap(NULL, sizeof(int)*NUM_PROC, protection, visibility, 0, 0);
     (*count) = 0;
     (*numero) = 0;
+
+    /*incializando os vetores com 0*/
     for(int i=0; i<NUM_PROC; i++){
         processo[i]=0;
         init[i] = 0;
     }
+
+    /*loop principal para pegar varios numeros*/
     while(scanf("%d%c", numero, &c)){
         
         tam++;
         
         for(int i=NUM_PROC-1; i>=0; i--){
+
+            //if para mudar entre qual processos e decidir qual sera o processo a executar a conta 
             if(!((tam%NUM_PROC)%(i+1))){
-                //printf("\ntam = %d, filho = %d\n", tam, i);
-                //printf("init = %d\n", init[i]);
+                //caso o processo ja tenha sido iniciado so preciso mandar um 'sinal' para que ele 
+                //execute a conta necessaria
                 if(!init[i]){
                     init[i] = 1;
                     filho[i] = fork();
-                    //printf("iniciei filho %d\n", i);
+                    //se for o processo filho o sinal entra num loop infinito e encerra qdo receber
+                    //um sinal de saida, 
                     if(!filho[i]){
                         signal(SIGQUIT, sigquit);
-                        //printf("entrei no filho %d\n", i);
                         while(1){
+                            //verificador do 'sinal' para fazer a conta
                             if(processo[i]){
                                 processo[i] = 0;
                                 (*count)+=primo(*numero);
-                                sleep(1);
+                                sleep(0.5);
                             }
                         }
+                    //se for o processo pai so envio o sinal para fazer a conta
                     }else{
-                        processo[i] = 1;
-                        sleep(2);
-                    }
                     
+                        processo[i] = 1;
+                        sleep(1);
+                    }
+                //se todos ja foram iniciados so enviar um 'sinal' para fazer a conta no processo i
                 }else if (all_proc(init)){
                     processo[i] = 1;
-                    sleep(2);
+                    sleep(1);
                 }
             break;
             }
         }
-        //printf("oioi\n");
+        //if de saida para \n
        if(c == '\n') break; 
     }
-
+    //loop para encerrar todos os processos
     for(int j=0; j<4; j++){
         if(init[j]) kill(filho[j], SIGQUIT);
         waitpid(filho[j], NULL, 0);}
+  //print do resultado final
   printf("%d\n", (*count));
   return 0;
 }
